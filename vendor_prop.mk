dd8c14b776e65ab285dacde733858dbc86cbc4c3
Nich <nctrenco@gmail.com>
2018-09-13 01:02:11 +0800

    ugglite: vendor_prop: Set virtual displays to 0
    We don't have any virtual displays, however HWC assumes 1 virtual
    display by default. This was causing laggy display when allocating
    virtual displays, as done by surfaceflinger during screen recording
    for example.
    
    Change-Id: Iab7ddb4182ee1d952f3083942eece033f651af98

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -102,7 +102,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.enable.sglscale=1 \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.mdpcomp.logs=0 \
-    debug.sdm.support_writeback=0 \
     debug.sf.hw=0 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
@@ -118,8 +117,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.perf_hint_window=50 \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.enable_default_color_mode=1 \
-    sys.display-size=1920x1080
-
+    sys.display-size=1920x1080 \
+    video.accelerate.hw=1 \
+    debug.sdm.support_writeback=0
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
