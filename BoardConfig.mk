8066a3a66be429bd9c921d680e95dfb9aa830c05
TheScarastic <warabhishek@gmail.com>
2019-01-30 18:16:42 +0530

    ugglite: Switch to more generic dt2w node
    Change-Id: I08c874f3ca50fad1db820ddd85ef0f5d7ccf0ed2
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>
    Signed-off-by: soekarnohatta <hidahhusni@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE := ignore_loglevel androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
@@ -141,9 +141,6 @@ TARGET_COMPILE_WITH_MSM_KERNEL	:= true
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
-
-# Tap2Wake
-TARGET_TAP_TO_WAKE_NODE := "/proc/gesture/onoff"
 
 # Malloc
 MALLOC_SVELTE := true
@@ -171,6 +168,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event2"
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
