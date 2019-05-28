505b6376c3b05fb960ce29bf75cbf61b58316bdb
Giovix92 <ggualtierone@gmail.com>
2019-05-28 16:29:45 +0200

    ugglite: dirty flash fix
    This helps after a dirty flash, in order to avoid broken strings on settings app

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -435,6 +435,9 @@ case "$target" in
         ;;
 esac
 
+# Remove settings cache, avoids derps after dirty flash
+rm -rf /data/system/package_cache
+
 #
 # Make modem config folder and copy firmware config to that folder for RIL
 #
