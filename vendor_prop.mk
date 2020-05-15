211e92b070615dbdb44c0b704fb1465f778ca3d4
Anusha Shakarad <ashaka@codeaurora.org>
2020-05-15 19:11:24 +0200

    ugglite: disable SF backpressure
    Change-Id: I3bacf63cdeab50be41306e8d58eed1c0d9316258

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -107,6 +107,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=0 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
+    debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
