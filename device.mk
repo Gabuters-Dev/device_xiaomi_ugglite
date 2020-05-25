35ae9627a7db3fc6eecda2515aabd3ecf6446035
mgs28-mh <55727892+mgs28-mh@users.noreply.github.com>
2020-05-25 15:19:56 +0700

    ugglite : change GoogleCameraGo to Snap

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8937 \
-    GoogleCameraGo
+    Snap
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
