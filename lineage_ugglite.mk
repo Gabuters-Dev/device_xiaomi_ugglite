206e54fca3104c5154cfa9ac31b01406e5a2dff9
andeh24 <fdlandriyanto@gmail.com>
2020-05-30 11:18:36 +0000

    Add Prebuilt APN list

--- lineage_ugglite.mk
+++ lineage_ugglite.mk
@@ -20,13 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-<<<<<<< HEAD:lineage_ugglite.mk
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-=======
-# Inherit some common Syberia stuff
-$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
->>>>>>> a264720... ugglite: Add prebuilt APN list:syberia_ugglite.mk
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
