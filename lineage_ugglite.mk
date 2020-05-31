b4f76e7f4a10241f494ac6e0ccd4d661cd9d59d1
garisku05 <garisku05@gmail.com>
2020-05-31 10:51:08 +0000

    ugglite:update lineage_ugglite.mk

--- lineage_ugglite.mk
+++ lineage_ugglite.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
