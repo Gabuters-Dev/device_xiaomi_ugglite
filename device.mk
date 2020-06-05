d71f2f151843cfe0c3a1a310bc86d6b2d0f4344e
Akbar Fatur Rochman <58977351+eevnxx-ai@users.noreply.github.com>
2020-06-05 18:01:23 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,8 +20,8 @@ DEVICE_PATH := device/xiaomi/ugglite
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
-    $(DEVICE_PATH)/overlay-mgs
+    $(DEVICE_PATH)/overlay-lineage \
+    $(DEVICE_PATH)/overlay-mgs \
 
 # Properties
 -include $(DEVICE_PATH)/vendor_prop.mk
