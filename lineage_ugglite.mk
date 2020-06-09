5ba455be86d22e3b7e036287284d44814f6d452a
adislice <romadhonadipermana279f@gmail.com>
2020-06-09 09:16:22 +0000

    ugglite: Use coral fingerprint for now
    Signed-off-by: adislice <romadhonadipermana279f@gmail.com>

--- lineage_ugglite.mk
+++ lineage_ugglite.mk
@@ -39,6 +39,6 @@ TARGET_VENDOR_PRODUCT_NAME := ugglite
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="ugglite" \
     PRODUCT_NAME="ugglite" \
-    PRIVATE_BUILD_DESC="ugglite-user 7.1.2 N2G47H V11.0.3.0.NDFMIXM release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200605.001 6392402 release-keys"
 
-BUILD_FINGERPRINT := "xiaomi/ugglite/ugglite:7.1.2/N2G47H/V11.0.3.0.NDFMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys"
