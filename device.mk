e358e304eb07c7f40f81540f4400121d9345de5d
ronaxdevil <pratabidya.007@gmail.com>
2020-06-16 14:30:33 +0530

    ugglite: sepolicy: add Spectrum Support
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com>
    Signed-off-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- device.mk
+++ device.mk
@@ -367,7 +367,9 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.spectrum.rc \
+    init.spectrum.sh \
 
 PRODUCT_PACKAGES += \
     init.qcom.sh \
