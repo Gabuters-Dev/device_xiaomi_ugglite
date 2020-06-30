4d3b8a73120ce6681504dac88747cc5375138711
garisku05 <garisku05@gmail.com>
2020-06-30 01:31:38 +0000

    Ugglite: cleanup

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -172,7 +172,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.sys.fw.bservice_enable=true \
     ro.vendor.qti.sys.fw.bservice_limit=5 \
     ro.vendor.qti.sys.fw.bservice_age=5000
 
