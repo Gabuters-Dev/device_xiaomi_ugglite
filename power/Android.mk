6d31f8accd8e40d6b0630e303e8946f4516972fb
adislice <romadhonadipermana279f@gmail.com>
2020-07-15 04:27:41 +0000

    ugglite: Update power HAL to 1.2 from LineageOS * Import QTI power HAL 1.2 from LineageOS * Add custom prefix to prevent duplicate definition
    Signed-off-by: adislice <romadhonadipermana279f@gmail.com>

--- power/Android.mk
+++ power/Android.mk
@@ -2,32 +2,70 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c power-8937.c
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
 
-LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libhidlbase \
+    libhidltransport \
+    libhwbinder \
+    libutils \
+    android.hardware.power@1.2
 
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    service.cpp \
+    Power.cpp
+
+LOCAL_CFLAGS += -Wall -Wextra -Werror
+
+ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
+    LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
+else
+
+# Include target-specific files.
+LOCAL_SRC_FILES += power-8937.c
+
+endif # End of board specific list
+
+ifneq ($(TARGET_POWER_SET_FEATURE_LIB),)
+    LOCAL_WHOLE_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
+endif
+
+ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
+    LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
+    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
 endif
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
 
-LOCAL_MODULE := power.qcom
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+ifeq ($(TARGET_ARCH),arm)
+    LOCAL_CFLAGS += -DARCH_ARM_32
+endif
+
+LOCAL_MODULE := android.hardware.power@1.2-service-qti.ugglite
+LOCAL_INIT_RC := android.hardware.power@1.2-service-qti.ugglite.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
+
+include $(BUILD_EXECUTABLE)
 
 endif
