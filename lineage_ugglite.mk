a90211847dcac26624f13a842cd52ef861045216
Heinz Der Flugel <heinzdf@outlook.com>
2020-07-16 13:35:02 +0000

    ugglite: correct build fingerprint
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- lineage_ugglite.mk
+++ lineage_ugglite.mk
@@ -41,6 +41,6 @@ TARGET_VENDOR_PRODUCT_NAME := ugglite
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="ugglite" \
     PRODUCT_NAME="ugglite" \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.005 6506677 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
