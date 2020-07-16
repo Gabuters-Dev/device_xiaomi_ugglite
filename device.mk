1e8eaa40d38ca1732b68050c47bb2cb4a61a3707
Albert I <kras@raphielgang.org>
2020-07-16 16:59:53 +0800

    ugglite: Merge gps_debug.conf into gps.conf
    Bind mount the latter instead since overriding targets isn't allowed anymore
    unless BUILD_BROKEN_DUP_RULES is set.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I64cbf2834a5be04d1a887ab1c13139d0a4de52c9
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- device.mk
+++ device.mk
@@ -195,11 +195,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(DEVICE_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(DEVICE_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(DEVICE_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(DEVICE_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(DEVICE_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(DEVICE_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf \
+    $(DEVICE_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
 # Healthd
 PRODUCT_PACKAGES += \
