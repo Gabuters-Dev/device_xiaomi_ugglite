fe380f9dc230040b8b5f8db12f0317cfdf19edca
Heinz Der Flugel <heinzdf@outlook.com>
2020-07-18 15:11:04 +0000

    Welcome DerpFest!
    Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ugglite.mk
+    $(LOCAL_DIR)/derp_ugglite.mk
 
 COMMON_LUNCH_CHOICES := \
-     lineage_ugglite-userdebug
+     derp_ugglite-userdebug
--- derp_ugglite.mk
+++ derp_ugglite.mk
@@ -20,11 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit from ugglite device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Vanilla
+DERP_BUILD_ZIP_TYPE := VANILLA
 
 TARGET_BOOT_ANIMATION_RES := 720
 
@@ -32,7 +35,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := ugglite
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 5A/Y1 Lite
-PRODUCT_NAME := lineage_ugglite
+PRODUCT_NAME := derp_ugglite
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
